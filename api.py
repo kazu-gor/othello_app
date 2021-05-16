841b083d52b9e4b520bded875bceaab14ff7fd2b
岩崎一輝 <kazu59@kazu-gor.local>
2021-05-17 08:42:04 +0900

    first commit

---
+++ api.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, abort, make_response
+import peewee
+
+class User(peewee.Model):
+    user_input = peewee.IntegerField()
+    
+api = Flask(__name__)
+
+@api.route('/', methods=['GET'])
+def get_user():
+    try:
+        sum
+    except NameError:
+        sum = 0
+    try:
+        sum += 3
+    except User.DoesNotExist:
+        abort(404)
+        
+    result = {
+        "data": {
+            "output": sum,
+        }
+    }
+    
+    return make_response(jsonify(result))
+
+@api.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port=3000)
