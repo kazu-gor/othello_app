89a81a8deb094a52fbc8c266ed4eb6bfda6327ce
岩崎一輝 <kazu59@kazu-gor.local>
2021-05-17 10:05:21 +0900

    change 'return'

--- api.py
+++ api.py
@@ -7,30 +7,32 @@ class User(peewee.Model):
 api = Flask(__name__)
 
 # @api.route('/', methods=['GET'])
-@api.route('/')
-def get_user():
-    try:
-        sum
-    except NameError:
-        sum = 0
-    try:
-        sum += 3
-    except User.DoesNotExist:
-        abort(404)
+# @api.route('/')
+# def get_user():
+#     try:
+#         sum
+#     except NameError:
+#         sum = 0
+#     try:
+#         sum += 3
+#     except User.DoesNotExist:
+#         abort(404)
         
-    result = {
-        "data": {
-            "output": sum,
-        }
-    }
-    
-    # return make_response(jsonify(result))
-    return result
+#     result = {
+#         "data": {
+#             "output": sum,
+#         }
+#     }
+#     # return make_response(jsonify(result))
+#     return result
 
-@api.errorhandler(404)
-def not_found(error):
-    # return make_response(jsonify({'error': 'Not found'}), 404)
-    return {'error': 'Not found'}
+# @api.errorhandler(404)
+# def not_found(error):
+#     # return make_response(jsonify({'error': 'Not found'}), 404)
+#     return {'error': 'Not found'}
+@api.route('/')
+def hello():
+    return 'Hello World'
 
 if __name__ == '__main__':
     api.run(host='0.0.0.0', port=3000, debug=True)
