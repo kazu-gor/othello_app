9b7f034a45decc30f9f015158f217ec80737648c
岩崎一輝 <kazu59@kazu-gor.local>
2021-05-17 10:35:13 +0900

    edit api.py

--- api.py
+++ api.py
@@ -14,45 +14,24 @@
 
 # [START gae_python38_app]
 # [START gae_python3_app]
+from flask import Flask
 
-from flask import Flask, jsonify, abort, make_response
-# import peewee
 
-# class User(peewee.Model):
-#     user_input = peewee.IntegerField()
-    
-api = Flask(__name__)
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
 
-# @api.route('/', methods=['GET'])
-# @api.route('/')
-# def get_user():
-#     try:
-#         sum
-#     except NameError:
-#         sum = 0
-#     try:
-#         sum += 3
-#     except User.DoesNotExist:
-#         abort(404)
-        
-#     result = {
-#         "data": {
-#             "output": sum,
-#         }
-#     }
-#     # return make_response(jsonify(result))
-#     return result
 
-# @api.errorhandler(404)
-# def not_found(error):
-#     # return make_response(jsonify({'error': 'Not found'}), 404)
-#     return {'error': 'Not found'}
-@api.route('/')
+@app.route('/')
 def hello():
-    return 'Hello World'
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
 
 if __name__ == '__main__':
-    api.run(host='127.0.0.1', port=8080, debug=True)
-    
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
 # [END gae_python3_app]
 # [END gae_python38_app]
