9243b9a8a6d13cb1bf15b75884f58305a52b8c79
岩崎一輝 <kazu59@kazu-gor.local>
2021-05-17 10:20:07 +0900

    change 'return'

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, abort, make_response
-import peewee
+# import peewee
 
-class User(peewee.Model):
-    user_input = peewee.IntegerField()
+# class User(peewee.Model):
+#     user_input = peewee.IntegerField()
     
 api = Flask(__name__)
 
@@ -35,4 +35,4 @@ def hello():
     return 'Hello World'
 
 if __name__ == '__main__':
-    api.run(host='0.0.0.0', port=3000, debug=True)
+    api.run(host='127.0.0.1', port=8080, debug=True)
