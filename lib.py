9ff2cf9212fc5b04d6a296063d7c9994ed8ce5e9
岩崎一輝 <kazu59@kazu-gor.local>
2021-05-20 23:25:56 +0900

    edited some content

--- lib.py
+++ lib.py
@@ -1,21 +1,13 @@
 import numpy as np
 
 def edit_value(value):
-    value = value.split(" ")
-    print(value)
-    array_list = []
-    for i in value:
-        print(i)
-        try:
-            array_list.append(int(i))
-        except ValueError:
-            pass
-    board = np.array(array_list)
-    print(board)
     try:
-        board = board.reshape(8, 8)
-    except ValueError:
-        return None
+        value = value.replace(" ", "").replace("[[", "")\
+            .replace("]]", "").replace("[", "").replace("]", "")
+        board = np.fromstring(value, dtype="int32", sep=",").reshape(8 ,8)
+    except Exception as e:
+        return e.message
+
     return board
     
     
